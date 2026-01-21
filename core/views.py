78515dee8eed48072d6982256033828e0ec55570
DavidCW2004 <dw716@exeter.ac.uk>
2026-01-21 16:54:17 +0000

    remove unused views and cached bytecode files

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render
-
-
-def home(request):
-	context = {
-		"user_count": User.objects.count(),
-	}
-	return render(request, "core/home.html", context)
